85e6a3f20eae8c419012e120e9854640065ee3fd
Elisa Crescentini <elisa@ruggedsoftware.dev>
2023-10-28 16:24:14 +0200

    starting project

---
+++ screens/LoginScreen.js
@@ -0,0 +1,10 @@
+//Libs
+
+//Local
+import AuthContent from "../components/Auth/AuthContent";
+
+function LoginScreen() {
+  return <AuthContent isLogin />;
+}
+
+export default LoginScreen;
---
+++ screens/SignupScreen.js
@@ -0,0 +1,10 @@
+//Libs
+
+//Local
+import AuthContent from "../components/Auth/AuthContent";
+
+function SignupScreen() {
+  return <AuthContent />;
+}
+
+export default SignupScreen;
