20ebaf53057fcea65211b2dab2eca5740c627adb
Elisa Crescentini <elisa@ruggedsoftware.dev>
2023-10-28 17:32:47 +0200

    use token for access protected resources

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,13 +1,33 @@
 //Libs
 import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../store/auth-context";
 
 //Local
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-expense-tra-f7db5-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
